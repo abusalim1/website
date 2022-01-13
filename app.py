267246ad5e7fd33837ae4ba12c4e06327d9bad59
abusalim1 <abusalimkv@gmail.com>
2022-01-13 14:57:46 +0530

    kuchh bhi

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template,request
 app=Flask(__name__)
 
-print(app)
+@app.route('/')
+def index():
+    name='OA730k9ilpI'
+    return render_template('index.html', vid=name)
 
-#@app.route('/')
-#def index():
-    #url= url("https://cdn.pixabay.com/photo/2016/12/19/18/21/snowflake-1918794_960_720.jpg");
-#   name='MUMCZZ19QCY'
- # return render_template('index.html', vid=name)
-
-#@app.errorhandler(404)
-#    return( render_template('404.html'), 404)
+@app.errorhandler(404)
+def page_not_found(e):
+    return  ( render_template('404.html'), 404)
 
 if __name__=='__main__':
     app.run(debug=True)
